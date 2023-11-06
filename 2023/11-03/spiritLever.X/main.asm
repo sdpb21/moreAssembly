c6157e1e1c8dbe408486f50048fb17d04b8cb8fe
sdpb21 <jytprf@gmail.com>
2023-11-06 18:43:40 -0300

    Configuration word added

--- 2023/11-03/spiritLever.X/main.asm
+++ 2023/11-03/spiritLever.X/main.asm
@@ -1,4 +1,13 @@
 ; TODO INSERT CONFIG CODE HERE USING CONFIG BITS GENERATOR
+; PIC16F684 Configuration Bit Settings
+
+; Assembly source line config statements
+
+#include "p16f684.inc"
+
+; CONFIG
+; __config 0xFFE5
+ __CONFIG _FOSC_INTOSCCLK & _WDTE_OFF & _PWRTE_ON & _MCLRE_ON & _CP_OFF & _CPD_OFF & _BOREN_ON & _IESO_ON & _FCMEN_ON
 
 RES_VECT  CODE    0x0000            ; processor reset vector
     GOTO    START                   ; go to beginning of program
