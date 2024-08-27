cb024dca2952864a604c554fa3d34c8aa9630391
sdpb21 <jytprf@gmail.com>
2024-08-27 19:38:06 -0300

    Code generated with Claude, now asking to use MIPS procedure call conventions (got stuck again.)

--- 2023/11-29/recFunct01.asm
+++ 2023/11-29/recFunct01.asm
@@ -22,9 +22,15 @@ main:
     syscall
 
 print_binary_patterns:
+    # Save the return address and frame pointer
+    addi $sp, $sp, -8
+    sw $ra, 4($sp)
+    sw $fp, 0($sp)
+    move $fp, $sp
+
     # Base case: if N is 0, print a newline and return
     beq $a0, $zero, print_newline
-    
+
     # Recursive case: print 0 and 1 for the current position
     li $t0, 0
     move $a0, $t0
@@ -32,11 +38,16 @@ print_binary_patterns:
     li $t0, 1
     move $a0, $t0
     jal print_bit
-    
+
     # Recursive call for the remaining positions
     addi $a0, $a0, -1
     jal print_binary_patterns
-    
+
+    # Restore the return address and frame pointer
+    lw $fp, 0($sp)
+    lw $ra, 4($sp)
+    addi $sp, $sp, 8
+
     # Return from the function
     jr $ra
 
@@ -45,12 +56,12 @@ print_bit:
     move $a0, $t0
     li $v0, 1
     syscall
-    
+
     # Print a space
     la $a0, newline
     li $v0, 4
     syscall
-    
+
     # Return from the function
     jr $ra
 
@@ -59,6 +70,6 @@ print_newline:
     la $a0, newline
     li $v0, 4
     syscall
-    
+
     # Return from the function
     jr $ra
