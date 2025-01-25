5a84217522015f8a2f6cf269ee313cce7f20a8f6
sdpb21 <jytprf@gmail.com>
2025-01-25 15:01:43 -0300

    commit

--- 2023/11-29/recFunct01.asm
+++ 2023/11-29/recFunct01.asm
@@ -1,7 +1,6 @@
 .data
 prompt: .asciiz "Enter the value of N: "
 newline: .asciiz "\n"
-
 .text
 .globl main
 main:
@@ -9,27 +8,26 @@ main:
     la $a0, prompt
     li $v0, 4
     syscall
-
     # Read the value of N from the user
     li $v0, 5
     syscall
     move $s0, $v0  # Store the value of N in $s0
-
     # Call the recursive function to print the binary patterns
     move $a0, $s0
     jal print_binary_patterns
     li $v0, 10
     syscall
 
-print_binary_patterns:
+
     # Save the return address and frame pointer
-    addi $sp, $sp, -8
+print_binary_patterns:    addi $sp, $sp, -8
     sw $ra, 4($sp)
     sw $fp, 0($sp)
     move $fp, $sp
 
     # Base case: if N is 0, print a newline and return
     beq $a0, $zero, print_newline
+    
 
     # Recursive case: print 0 and 1 for the current position
     li $t0, 0
@@ -38,10 +36,12 @@ print_binary_patterns:
     li $t0, 1
     move $a0, $t0
     jal print_bit
+    
 
     # Recursive call for the remaining positions
     addi $a0, $a0, -1
     jal print_binary_patterns
+    
 
     # Restore the return address and frame pointer
     lw $fp, 0($sp)
@@ -51,25 +51,28 @@ print_binary_patterns:
     # Return from the function
     jr $ra
 
-print_bit:
+
     # Print the current bit
-    move $a0, $t0
+print_bit:    move $a0, $t0
     li $v0, 1
     syscall
+    
 
     # Print a space
     la $a0, newline
     li $v0, 4
     syscall
+    
 
     # Return from the function
     jr $ra
 
-print_newline:
+
     # Print a newline
-    la $a0, newline
+print_newline:    la $a0, newline
     li $v0, 4
     syscall
+    
 
     # Return from the function
     jr $ra
