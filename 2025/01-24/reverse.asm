015e7552e742f25bcf3dc569dac729eeaeb6d56d
sdpb21 <jytprf@gmail.com>
2025-01-25 23:40:00 -0300

    Infinite execution problem solved

--- 2025/01-24/reverse.asm
+++ 2025/01-24/reverse.asm
@@ -13,9 +13,12 @@
 ; gdb reverse
 
 section .data
+
 ; We'll use these for output
 newline: db 10, 0 ; A single newline character
+
 section .bss
+
 buf_in: resb 256 ; Buffer for reading lines
 s1: resb 128 ; Buffer for first reversed number
 s2: resb 128 ; Buffer for second reversed number
@@ -29,8 +32,7 @@ global _start
 ; Return value: number of bytes read in EAX
 ; -------------------------------------------------------------------------
 
-read_line:
-	mov eax, 3	; sys_read
+read_line: mov eax, 3	; sys_read
 	mov ebx, 0	; stdin (file descriptor 0)
 	mov ecx, buf_in
 	mov edx, 255
@@ -41,9 +43,8 @@ read_line:
 ; print_string( ecx=pointer, edx=length )
 ; -------------------------------------------------------------------------
 
-print_string:
-	mov eax, 4	; sys_write
-	mov ebx, 1	; stdout
+print_string: mov eax, 4	; sys_write
+	mov ebx, 1		; stdout
 	int 0x80
 	ret
 
@@ -51,9 +52,8 @@ print_string:
 ; print_newline()
 ; -------------------------------------------------------------------------
 
-print_newline:
-	mov eax, 4	; sys_write
-	mov ebx, 1	; stdout
+print_newline: mov eax, 4	; sys_write
+	mov ebx, 1		; stdout
 	mov ecx, newline
 	mov edx, 1
 	int 0x80
@@ -68,14 +68,11 @@ print_newline:
 ; becomes "123". If everything is zeros, leave just one '0'.
 ; -------------------------------------------------------------------------
 
-remove_leading_zeros_in_reversed:
-	push ebp
+remove_leading_zeros_in_reversed: push ebp
 	mov ebp, esp
 	mov esi, buf_sum
 
-.strip_loop:
-	; If we've reached the end (all zeros?), break
-	mov al, [esi]
+.strip_loop: mov al, [esi] ; If we've reached the end (all zeros?), break
 	cmp al, 0
 	je .all_zero	; we found 0 terminator => entire string was empty
 	cmp al, '0'
@@ -84,22 +81,18 @@ remove_leading_zeros_in_reversed:
 				; We'll shift everything from [esi+1..end] left by one
 	mov edi, esi
 
-.shift_left:
-	mov al, [edi + 1]
+.shift_left: mov al, [edi + 1]
 	mov [edi], al
 	cmp al, 0
 	je .strip_loop		; once we place 0, start over
 	inc edi
 	jmp .shift_left
 
-.all_zero:
-	; That means all got stripped => set the string to "0"
-	mov byte [esi], '0'
+.all_zero: mov byte [esi], '0' ; That means all got stripped => set the string to "0"
 	mov byte [esi+1], 0
 	jmp .done_stripping
 
-.done_stripping:
-	pop ebp
+.done_stripping: pop ebp
 	ret
 
 ; -------------------------------------------------------------------------
@@ -120,8 +113,7 @@ remove_leading_zeros_in_reversed:
 ; buf_sum[i] = 0
 ; -------------------------------------------------------------------------
 
-add_reversed:
-	push ebp
+add_reversed: push ebp
 	mov ebp, esp
 	; ESI -> s1, EDI -> s2, EBX -> buf_sum
 	mov esi, s1
@@ -129,30 +121,23 @@ add_reversed:
 	mov ebx, buf_sum
 	xor ecx, ecx ; ecx = carry
 
-.add_loop:
-	; Read digit from s1
-	mov al, [esi]
+.add_loop: mov al, [esi] ; Read digit from s1
 	cmp al, 0
 	je .use_zero_s1
 	sub al, '0'		; convert from ascii
 	jmp .got_d1
 
-.use_zero_s1:
-	xor eax, eax
+.use_zero_s1: xor eax, eax
 
-.got_d1:
-	; Read digit from s2
-	mov dl, [edi]
+.got_d1: mov dl, [edi] ; Read digit from s2
 	cmp dl, 0
 	je .use_zero_s2
 	sub dl, '0'
 	jmp .got_d2
 
-.use_zero_s2:
-	xor edx, edx
+.use_zero_s2: xor edx, edx
 
-.got_d2:
-	add al, dl		; d1 + d2
+.got_d2: add al, dl		; d1 + d2
 	add al, cl		; + carry in ecx's low byte
 				; sum is now in AL
 				; Compute new carry
@@ -162,45 +147,33 @@ add_reversed:
 	mov cl, 1		; carry = 1
 	jmp .store_digit
 
-.no_carry:
-	xor ecx, ecx		; carry = 0
+.no_carry: xor ecx, ecx		; carry = 0
 
-.store_digit:
-	add al, '0'
+.store_digit: add al, '0'
 	mov [ebx], al		; store result digit in buf_sum
 	inc ebx
 
-	; Advance pointer in s1 if not at zero
-	cmp byte [esi], 0
+	cmp byte [esi], 0	; Advance pointer in s1 if not at zero
 	je .skip_inc_s1
 	inc esi
 
-.skip_inc_s1:
-	; Advance pointer in s2 if not at zero
-	cmp byte [edi], 0
+.skip_inc_s1: cmp byte [edi], 0 ; Advance pointer in s2 if not at zero
 	je .skip_inc_s2
 	inc edi
 
-.skip_inc_s2:
-	; Check if we should continue:
-	; We continue if either s1[i] or s2[i] is nonzero OR carry != 0
-	mov al, [esi]
-	mov dl, [edi]
+.skip_inc_s2: mov al, [esi]	; Check if we should continue:
+	mov dl, [edi]		; We continue if either s1[i] or s2[i] is nonzero OR carry != 0
 	or al, dl		; if both are zero => result = 0 in AL
 	or al, cl		; also check carry
 	jne .add_loop
 
-	; If carry is still set after we exit, we add one more digit
-	cmp cl, 0
+	cmp cl, 0		; If carry is still set after we exit, we add one more digit
 	je .done_adding
 
-	; carry = 1 => put one more '1'
-	mov byte [ebx], '1'
+	mov byte [ebx], '1'	; carry = 1 => put one more '1'
 	inc ebx
 
-.done_adding:
-	; Null-terminate
-	mov byte [ebx], 0
+.done_adding: mov byte [ebx], 0	; Null-terminate
 	pop ebp
 	ret
 
@@ -213,23 +186,19 @@ add_reversed:
 ; we just store them as-is into s1, s2.
 ; -------------------------------------------------------------------------
 
-parse_two_reversed:
-	push ebp
+parse_two_reversed: push ebp
 	mov ebp, esp
 
-	; Zero out s1 and s2 first
-	mov edi, s1
+	mov edi, s1	; Zero out s1 and s2 first
 	mov ecx, 128
 
-.clear_s1:
-	mov byte [edi], 0
+.clear_s1: mov byte [edi], 0
 	inc edi
 	loop .clear_s1
 	mov edi, s2
 	mov ecx, 128
 
-.clear_s2:
-	mov byte [edi], 0
+.clear_s2: mov byte [edi], 0
 	inc edi
 	loop .clear_s2
 
@@ -237,8 +206,7 @@ parse_two_reversed:
 	mov esi, buf_in		; read pointer
 	mov edi, s1		; write pointer for s1
 
-.next_char_s1:
-	mov al, [esi]
+.next_char_s1: mov al, [esi]
 	cmp al, ' '
 	je .done_s1
 	cmp al, 10
@@ -250,18 +218,13 @@ parse_two_reversed:
 	inc esi
 	jmp .next_char_s1
 
-.done_s1:
-	; skip the space if that was a space
-	cmp al, ' '
+.done_s1: cmp al, ' '		; skip the space if that was a space
 	jne .skip_space
 	inc esi
 
-.skip_space:
-	; now parse s2
-	mov edi, s2
+.skip_space: mov edi, s2	; now parse s2
 
-.next_char_s2:
-	mov al, [esi]
+.next_char_s2: mov al, [esi]
 	cmp al, 10
 	je .done_s2
 	cmp al, 0
@@ -271,8 +234,7 @@ parse_two_reversed:
 	inc esi
 	jmp .next_char_s2
 
-.done_s2:
-	pop ebp
+.done_s2: pop ebp
 	ret
 
 ; -------------------------------------------------------------------------
@@ -281,27 +243,23 @@ parse_two_reversed:
 ; the integer result in EBX.
 ; -------------------------------------------------------------------------
 
-convert_string_to_int_in_EBX:
-	push ebp
+convert_string_to_int_in_EBX: push ebp
 	mov ebp, esp
 	mov ebx, 0	; EBX will hold the integer
 	mov esi, buf_in
 
-.next_char:
-	mov al, [esi]
+.next_char: mov al, [esi]
 	cmp al, 10	; newline?
 	je .done
 	cmp al, 0
 	je .done
 	sub al, '0'
-	; EBX = EBX*10 + (al)
-	imul ebx, ebx, 10
+	imul ebx, ebx, 10	; EBX = EBX*10 + (al)
 	add ebx, eax
 	inc esi
 	jmp .next_char
 
-.done:
-	pop ebp
+.done: pop ebp
 	ret
 
 ; -------------------------------------------------------------------------
@@ -309,22 +267,18 @@ convert_string_to_int_in_EBX:
 ; Print buf_sum as is (which is the reversed sum after zero-stripping),
 ; then print a newline.
 ; -------------------------------------------------------------------------
-print_buf_sum:
-	push ebp
+print_buf_sum: push ebp
 	mov ebp, esp
 	mov esi, buf_sum
 	xor edx, edx ; length in EDX
 
-.len_loop:
-	mov al, [esi + edx]
+.len_loop: mov al, [esi + edx]
 	cmp al, 0
 	je .got_len
 	inc edx
 	jmp .len_loop
 
-.got_len:
-	; Now EDX = length
-	mov ecx, buf_sum
+.got_len: mov ecx, buf_sum	; Now EDX = length
 	mov eax, 4 ; sys_write
 	mov ebx, 1
 	int 0x80
@@ -343,15 +297,14 @@ _start:
 	call convert_string_to_int_in_EBX	; result in EBX
 	mov esi, ebx				; store N in ESI (we'll decrement ESI each loop)
 
-.loop_cases:
-	cmp esi, 0
+.loop_cases: cmp esi, 0
+	push esi
 	je .done_all
 
 	; read next line
 	call read_line
 
-	; if EAX <= 0, no more input (unexpected), just end
-	cmp eax, 1
+	cmp eax, 1	; if EAX <= 0, no more input (unexpected), just end
 	jl .done_all
 
 	; parse into s1, s2
@@ -365,11 +318,10 @@ _start:
 
 	; print the result
 	call print_buf_sum
+	pop esi
 	dec esi
 	jmp .loop_cases
 
-.done_all:
-	; Normal exit
-	mov eax, 1
+.done_all: mov eax, 1		; Normal exit
 	xor ebx, ebx
 	int 0x80
