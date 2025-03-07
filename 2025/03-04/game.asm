3b690f3592e5280dbf45ca801f837f3122c69474
sdpb21 <jytprf@gmail.com>
2025-03-07 05:36:46 -0300

    Field first version working perfectly

--- 2025/03-04/game.asm
+++ 2025/03-04/game.asm
@@ -1,16 +1,33 @@
 .data
 
-score:	.asciiz	"Score: 0"
+score:	.asciiz	"Score: 0\n"
+field:	.asciiz	"#########\n"
+	.asciiz	"#########\n"
+	.asciiz	"#########\n"
+	.asciiz	"#########\n"
+	.asciiz	"#########\n"
+	.asciiz	"#########\n"
+	.asciiz	"#########\n!"
 
 .text
 
 	li $s0, 0xffff0000	# load the base address of MMIO area in $s0
 
 	# printing the score
-printScore: lb $a0, score($t1)	# load the byte on $t1 position in $a0
+	andi $t1, 0		# clear $t1
+printScore: lb $a0, score($t1)	# load the byte on $t1 position from score, in $a0
 	jal displayReady	# print the byte loaded on MMIO display
 	addi $t1, $t1, 1	# increments $t1 to go for the next byte in score string
 	bne $a0, $zero, printScore # if char is not null, repeat
+
+	# printing the field
+	andi $t1, 0		# clear $t1 again
+printField: lb $a0, field($t1)	# load the byte on $t1 position from field, in $a0
+	beq $a0,'!', fieldEnd	# field end byte found, do not print, jump to fieldEnd label
+	jal displayReady	# print the byte loaded on MMIO display
+	addi $t1, $t1, 1	# go to the next byte to print
+fieldEnd: bne $a0, '!', printField # if byte isn't '!', repeat
+
 	j stop			# jumps to stop label to stop the program
 
 	# to print on MMIO display
